2bd0ff01ed1665be3318156e9281f7cb9c8962b6
lttinCS <162774530+tinletrongCSE@users.noreply.github.com>
2026-04-07 15:19:32 +0700

    refactored

--- backend/core/security.py
+++ backend/core/security.py
@@ -21,7 +21,7 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
-    """Tạo chuối JWT string"""
+    """Tạo chuỗi JWT string"""
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
